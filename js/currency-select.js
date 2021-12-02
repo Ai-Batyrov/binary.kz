76d71332e28433da6008de6330f61d810d3cf8a8
Ai-Batyrov <aibolat.batyrov.03@gmail.com>
2021-12-02 20:49:54 +0600

    main

---
+++ js/currency-select.js
@@ -0,0 +1,68 @@
+const selector = document.querySelector("select");
+var getPrice = document.querySelectorAll(".price");
+
+function convertCurrency() {
+    for (let i = 0; i < getPrice.length; i++) {
+        let getPriceInnerHTML = getPrice[i].innerHTML;
+
+        if (selector.value == 'KZT') {
+            getPrice[i].innerHTML = "₸" +
+                toKZT(getPriceInnerHTML);
+        } else if (selector.value == 'EUR') {
+            getPrice[i].innerHTML = "€" +
+                toEUR(getPriceInnerHTML);
+        } else if (selector.value == 'USD') {
+           getPrice[i].innerHTML = "$" +
+                toUSD(getPriceInnerHTML);
+        }
+    }
+}
+
+
+
+function toKZT(value) {
+    let convertedValue;
+
+    if (value.includes("$")) {
+        value = value.replace("$","");
+        convertedValue = value * 430;
+    } else if (value.includes("€")) {
+        value = value.replace("€", "");
+        convertedValue = value * 500;
+    }
+
+    return convertedValue;
+}
+
+
+function toEUR(value) {
+    let convertedValue;
+
+    if (value.includes("$")) {
+        value = value.replace("$", "");
+        convertedValue = value / 1,13;
+    } else if (value.includes("₸")) {
+        value = value.replace("₸", "");
+        convertedValue = value / 500
+    }
+
+    return convertedValue;
+}
+
+
+function toUSD(value) {
+    let convertedValue;
+
+    if (value.includes("€")) {
+        value = value.replace("€", "");
+        convertedValue = value * 1,13;
+    } else if (value.includes("₸")) {
+        value = value.replace("₸", "");
+        convertedValue = value / 430;
+    } else if (value.includes("$")) {
+        value = value.replace("$","");
+        convertedValue = value;
+    }
+
+    return convertedValue;
+}
